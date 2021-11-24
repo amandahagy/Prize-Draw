b32993a4bf9344e75c343e65fefd55013a98713a
Amanda Hagy <amandahagy@MacBook-Air-de-Amanda.local>
2021-11-24 16:49:08 -0300

    GETs

--- contract.sol
+++ contract.sol
@@ -3,4 +3,17 @@ pragma solidity 0.4.24; // versions < 0.4.24 have constructor() issues
 contract Draw {
     address private contractManager;  // variable
     address[] private players;    // variable
+
+    function getPlayers() public view returns (address[]){
+        return players;
+    }
+
+    function getContractManager() public view returns (address) {
+        return contractManager;
+    }
+
+    function getBalance() public view returns (uint)
+    {
+        return address(this).balance;
+    }
 }
